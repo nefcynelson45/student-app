aca4cc3938f1971fa284e07d92fa15fd12ff98a5
nefcynelson45 <nefcynelson@gmail.com>
2021-03-17 11:53:34 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student App</title>
+</head>
+<body>
+    <div class="container">
+    <div class="col-lg-4">
+        <table class="table table-borderless">
+        <tr>
+            <td><input type="text" class="form-control" placeholder="Name"></td>
+        </tr>
+        <tr>
+            <td><input type="text" class="form-control" placeholder="RollNo"></td>
+        </tr>
+        <tr>
+            <td><input type="text" class="form-control" placeholder="Admission No."></td>
+        </tr>
+        <tr>
+            <td><input type="text" class="form-control" placeholder="College"></td>
+        </tr>
+        <tr>
+            <td><button class="btn btn-primary">Register</button></td>
+        </tr>
+        </table>
+    </div>
+    </div>
+</body>
+</html>
